4c5481fdd5dc010c2f706f30091090581f96d771
Marcela Guerrero <marcelaguerrero1396@gmail.com>
2020-10-15 12:01:02 -0500

    Mi primer commit del proyecto repaso

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Repaso Seminario </title>
+	<!-- Archivos de estilos --> 
+
+</head>
+<body>
+
+	<h1> HOLA MUNDO  </h1>
+
+	<!-- Archivos Javascript -->
+
+</body>
+</html>
